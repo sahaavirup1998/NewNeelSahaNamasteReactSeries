0e418bb3f29c4b69ed9b159599ddba4f2076c941
Avirup Saha <avirup.saha@epsilon.com>
2024-01-01 22:44:09 +0530

    Episode1

---
+++ Episode1/App.js
@@ -0,0 +1,16 @@
+// const heading = React.createElement("h1", {id:"heading"}, "Welcome to React by react!");
+// reactelement is an object and becomes a html for browser to understand
+const parent = React.createElement(
+    "div", {id:"parent"}, [
+        React.createElement("div", {id:"chid"}, [
+            React.createElement("h1", {}, "I am the new heading"),
+            React.createElement("h2", {}, "I am the new heading2")]
+        ),
+        React.createElement(
+            "div", {id:"chid2"}, [
+                React.createElement("h1", {}, "I am the new heading3"),
+                React.createElement("h2", {}, "I am the new heading4")]
+        )]
+    );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
